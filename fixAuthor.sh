831b314d017e2d8398ab434856ebd149debfa92c
Helon R <rhelon@gmail.com>
2021-07-02 11:33:24 +0200

    Create fixAuthor.sh

---
+++ fixAuthor.sh
@@ -0,0 +1,15 @@
+git filter-branch --env-filter '
+OLD_EMAIL="{your old email here}"
+CORRECT_NAME="{your correct username here}"
+CORRECT_EMAIL="{your correct email here}"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
